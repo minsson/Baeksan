1d78ff26b5426401563975d54ba063d5ddf72a35
minsson <mskaang@gmail.com>
2022-09-14 12:40:16 +0900

    feat: Mountain 모델 데이터 구현
    - 산 정보를 parsing하여 담기 위함

--- Baeksan/Baeksan/Models/Mountain.swift
+++ Baeksan/Baeksan/Models/Mountain.swift
@@ -5,23 +5,21 @@
 //  Created by minsson on 2022/09/14.
 //
 
-import Foundation
+import CoreLocation
 
-//TODO: - Mountain 모델 데이터 구현
+struct Mountain: Decodable {
+    let title: String
+    let subtitle: String // 산부제정보
+    let height: Int
+    let address: String
+    let reason: String // 100대 명산 선정 이유
+    let overview: String // 산 정보 개요 상세내용
+    let description: String // 산 추가설명
+    let transportation: String // 교통정보
+    let location: Coordinate
+}
 
-
-import Foundation
-
-struct Content: Decodable {
-    let name: String
-    let imageName: String
-    let shortDescription: String
-    let description: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case name
-        case imageName = "image_name"
-        case shortDescription = "short_desc"
-        case description = "desc"
-    }
+struct Coordinate: Codable {
+    let latitude: CLLocationDegrees
+    let longitude: CLLocationDegrees
 }
