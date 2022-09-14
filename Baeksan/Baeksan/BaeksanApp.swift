5b2f77b7d227b24295b6c97e6b6823217a08e209
minsson <mskaang@gmail.com>
2022-09-14 11:48:08 +0900

    feat: Baeksan 프로젝트 파일 생성

---
+++ Baeksan/Baeksan/BaeksanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaeksanApp.swift
+//  Baeksan
+//
+//  Created by minsson on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct BaeksanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
